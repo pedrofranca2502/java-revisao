87e18b17d8dea244ea033e520ba0cfdd61d10652
pedrofranca2502 <pedrofranca250204@gmail.com>
2022-03-25 18:25:43 -0300

    codigo completo

---
+++ java.js
@@ -0,0 +1,16 @@
+const input=require("readline-sync")
+
+
+function soma(numero1,numero2){
+    return numero1 + numero2
+}
+
+console.log(soma(2,4))
+
+
+
+function soma(numero1,numero2){
+    console.log(numero1+numero2)
+}
+
+soma(2,4)
